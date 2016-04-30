d46c9513fe52a03c6efdcb4d11a68370e8e3ecc3
Miquel A. Muntaner Morey <m.muntaner.uoc@gmail.com>
2016-04-30 16:35:49 +0200

    Dummy Server with some dummy endPoints for testing functionality v2

--- server/routes/games.js
+++ server/routes/games.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET games listing. */
 router.get('/', function(req, res, next) {
   res.send('GET /GAMES   respond with a resource');
+});
+
+/* GET Game users listing. */
+router.get('/users', function(req, res, next) {
+  res.send('GET /GAMES/USERS   respond with a resource');
 });
 
 module.exports = router;
